311f5f207b235b443c71909e87c86e7e1dfbea0f
Gopakumar <132288346+Devopslearner2023@users.noreply.github.com>
2024-04-22 15:29:29 +0530

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -2,4 +2,8 @@
 
 sudo apt-get update -y
 sudo apt install nginx -y
-sudo systemctl start nginx
+cd /var/www/html && wget https://www.tooplate.com/zip-templates/2131_wedding_lite.zip
+sudo apt install unzip
+unzip 2131_wedding_lite.zip
+sudo cp -pr 2131_wedding_lite /var/www/html/
+sudo systemctl restart nginx
