422102c8b1803a9afe83a02896c4dce45ad7295d
Gopakumar <132288346+Devopslearner2023@users.noreply.github.com>
2024-04-24 09:59:35 +0530

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 sudo apt-get update -y
-sudo apt install unzip -y
 sudo apt install nginx -y
-cd /var/www/html && wget https://www.tooplate.com/zip-templates/2133_moso_interior.zip
-sudo unzip 
+echo "Welcome to Nginx-" > /var/www/html/index.html
 sudo systemctl restart nginx
